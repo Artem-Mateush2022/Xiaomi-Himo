1e5316029c3531b79cb72b909a117277c81541c7
Artem-Mateush2022 <103309654+Artem-Mateush2022@users.noreply.github.com>
2022-05-23 19:17:41 +0300

    Add files via upload

---
+++ xiaomi humo/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".bike__slider,.slider__items").slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    })
+})
